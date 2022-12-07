5d93da6b625b2e0cdebf70589d11c8e5872fc096
mfrabott <mfrabott14@gmail.com>
2022-12-07 16:47:19 -0500

    day 7

--- 007/index1.js
+++ 007/index1.js
@@ -1,23 +1,63 @@
-
 const fs = require('fs');
-const data = fs.readFileSync('data1.txt', 'utf8');
+const data = fs.readFileSync('data.txt', 'utf8');
 const lines = data.split(/\n/);
 
-let currentCalories = 0;
-let mostCalories = 0;
+const directoryObjs = [];
 
-for (i=0; i<=lines.length; i++){
-    if (lines[i]===''){
-        if (currentCalories > mostCalories){
-            mostCalories = currentCalories;
-            currentCalories = 0;
-        } else {
-            currentCalories = 0;
-        }
-    } else {
-        currentCalories += Number(lines[i]);
-    }
-}
+// loop through system
+for (i=0; i<lines.length; i++){
+    // keep track of when inside a directory
+    let layers = 0
+    
+    // lets us know we're in a directory
+    if (lines[i].includes('$ cd') && lines[i].includes('$ cd ..') === false) {
+        layers ++;
 
-console.log(mostCalories)
+        // remove '$ cd " to leave dir name 
+        let dir = lines[i].slice(5);
+        let dirValue=0;
+        
+        // start looking through the dir for commands, dirs, and files...
+        for (j=i+1; j<lines.length; j++){ 
 
+            // ...so long as we're inside a directory
+            if (layers > 0) {
+            
+                // keep track of stepping out of a dir
+                if (lines[j].includes('$ cd ..')) {
+                    layers --;
+                
+                 // keep track of stepping into a directory
+                } else if (lines[j].includes('$ cd') && lines[j].includes('$ cd ..') === false) {
+                    layers ++;
+                
+                // grab the file size and add to current value
+                } else if (lines[j].includes('$ cd') === false && lines[j].includes('$ cd ..') === false) {
+                    let fileSize = Number(lines[j].match(/[+]?[0-9]*\.?[0-9]+/g));
+                    dirValue += fileSize;
+                }; 
+            
+                // create object with dir name and size
+            } else {
+                let dirObj = {
+                    dir:dir,
+                    value: dirValue
+                };
+
+                // save object if <= 1000000
+                if (dirObj.value<=100000) {
+                    directoryObjs.push(dirObj);
+                    break;
+                };
+            };
+        };
+    };
+};
+
+let total = 0
+// sum value of all directories<100000
+for (i=0; i<directoryObjs.length; i++){
+    total += directoryObjs[i].value
+    };
+
+console.log(total);
--- 007/index2.js
+++ 007/index2.js
@@ -1,35 +1,86 @@
-
 const fs = require('fs');
 const data = fs.readFileSync('data.txt', 'utf8');
 const lines = data.split(/\n/);
 
-let currentCalories = 0;
-let mostCalories = 0;
-let secondMost = 0;
-let thirdMost = 0;
+const directoryObjs = [];
 
+let rootValue = 0;
 
-
-for (i=0; i<=lines.length; i++){
-    if (lines[i]===''){
-        if (currentCalories > mostCalories){
-            mostCalories = currentCalories;
-            currentCalories = 0;
-        } else if (currentCalories>secondMost) {
-            secondMost = currentCalories;
-            currentCalories = 0;
-        } else if (currentCalories>thirdMost) {
-            thirdMost = currentCalories;
-            currentCalories = 0;
-        } else {
-            currentCalories = 0;
-        }
-    } else {
-        currentCalories += Number(lines[i]);
-    }
+// find file size of root directory
+for (i=0; i<lines.length; i++){
+    let fileSize = Number(lines[i].match(/[+]?[0-9]*\.?[0-9]+/g));
+    rootValue += fileSize;
 };
-let topThree = mostCalories+secondMost+thirdMost
-console.log(mostCalories)
-console.log(secondMost)
-console.log(thirdMost)
-console.log(topThree)
+
+let unusedSpace = 70000000 - rootValue;
+let neededSpace = 30000000 - unusedSpace;
+
+// loop through file system
+for (i=0; i<lines.length; i++){
+    
+    // keep track of when inside a directory
+    let layers = 0;
+
+    // lets us know we're in a directory
+    if (lines[i].includes('$ cd') && lines[i].includes('$ cd ..') === false) {
+        layers ++;
+
+        // remove '$ cd " to leave dir name 
+        let dir = lines[i].slice(5);
+        let dirValue=0;
+        
+        // start looking through the dir for commands, dirs, and files...
+        for (j=i+1; j<lines.length; j++){ 
+
+            // ...so long as we're inside a directory  
+            if (layers > 0) {
+
+                // keep track of stepping out of a dir
+                if (lines[j].includes('$ cd ..')) {
+                    layers --;
+                    
+                 // keep track of stepping into a directory
+                } else if (lines[j].includes('$ cd') && lines[j].includes('$ cd ..') === false) {
+                    layers ++;
+
+                // grab the file size and add to current value
+                } else if (lines[j].includes('$ cd') === false && lines[j].includes('$ cd ..') === false) {
+                    let fileSize = Number(lines[j].match(/[+]?[0-9]*\.?[0-9]+/g))
+                    dirValue += fileSize
+                };
+            
+            // create object with dir name and size
+            } else {
+                let dirObj = {
+                    dir:dir,
+                    value: dirValue
+                };
+
+            // save object
+            directoryObjs.push(dirObj);
+            break;
+            };
+        };
+    };
+};
+
+// sort function
+function compare( a, b ) {
+    if ( a.value < b.value ){
+      return -1;
+    }
+    if ( a.value > b.value ){
+      return 1;
+    }
+    return 0;
+  }
+  
+let sortedDirectories = directoryObjs.sort( compare );
+
+// find first directory greater than needed space
+for (i=0; i<sortedDirectories.length; i++) {
+    if (sortedDirectories[i].value >= neededSpace){
+        console.log(sortedDirectories[i])
+        break
+    };
+};
